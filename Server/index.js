8c7867d4a9e8d614cce42e50b5969279f29bcb86
Minalb2006 <minalborkar0012@gmail.com>
2025-12-25 17:50:55 +0530

    backend added

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,12 @@ dotenv.config();
 
 const app = e();
 const PORT = 5000;
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const User = require("./user");
+
 
 app.post("/hi", (req,res)=>{
     res.send("hello from server");
@@ -13,4 +19,60 @@ app.post("/hi", (req,res)=>{
 
 app.listen(PORT,()=>{
     console.log(`your server is running on port ${PORT}`)
+});
+
+
+
+//Minal code
+
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/paws_and_pet",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.log(err));
+
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+
+app.post("/adopt", async (req, res) => {
+  try {
+    const { username, email, petName, petWeight } = req.body;
+
+    const newUser = new User({
+      username,
+      email,
+      adoptedPet: {
+        petName,
+        petWeight,
+      },
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      message: "Adoption data saved successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error saving data",
+      error,
+    });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
