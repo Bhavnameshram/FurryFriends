a0e0b24839f938b3390ff0219a4bfd52b8fb53a3
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-01-03 14:29:40 +0530

    done

--- Client/vite-project/src/Page/Homemain.jsx
+++ Client/vite-project/src/Page/Homemain.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home1 from '../Components/Bhome1'
 import Home2 from '../Components/Bhome2'
 import Home3 from '../Components/Bhome3'
+import Home4 from '../Components/Bhomef'
 
 function Homemain() {
   return (
@@ -9,6 +10,7 @@ function Homemain() {
       <Home1/>
       <Home2/>
       <Home3/>
+      <Home4/>
     </>
   )
 }
